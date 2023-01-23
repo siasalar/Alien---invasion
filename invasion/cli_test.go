ce97068647e3ce76154d13bbdfb819eaa68189b8
siasalar <siavash.salarzehi@gmail.com>
2023-01-23 22:38:41 +0100

    Added cli_test

---
+++ invasion/cli_test.go
@@ -0,0 +1,48 @@
+package invasion
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestGetNumberOfAliens(t *testing.T) {
+	testCases := []struct {
+		name        string
+		args        []string
+		expectedNum int
+		expectedErr error
+	}{
+		{
+			name:        "valid input",
+			args:        []string{"invasion", "5"},
+			expectedNum: 5,
+			expectedErr: nil,
+		},
+		{
+			name:        "invalid input",
+			args:        []string{"invasion", "not_a_number"},
+			expectedNum: 0,
+			expectedErr: fmt.Errorf("invalid number of aliens: not_a_number"),
+		},
+		{
+			name:        "no input",
+			args:        []string{"invasion"},
+			expectedNum: 0,
+			expectedErr: fmt.Errorf("please provide the number of aliens as a command line argument"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			os.Args = tc.args
+			numAliens, err := GetNumberOfAliens()
+
+			require.Equal(t, tc.expectedNum, numAliens)
+
+			if err != nil {
+				require.Contains(t, err.Error(), tc.expectedErr.Error())
+			}
+		})
+	}
+}
